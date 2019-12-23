24e55a5c9a60a3652e2ce3a026250339ac0a82af
DannyCork <rootuid@gmail.com>
2019-12-23 13:06:36 +0000

    does all

--- generateDomains.py
+++ generateDomains.py
@@ -1,13 +1,19 @@
+#/usr/bin/python3
+
 import click
 from itertools import product
-from string import ascii_lowercase
+from string import ascii_lowercase, digits
 
 @click.command()
 @click.option('-tld', prompt='tld', help='tld.')
 @click.option('-size', prompt='size to generate', help='size of domain names to generate.')
 
 def domains_by_size (size, tld):
-    keywords = [''.join(i) for i in product(ascii_lowercase, repeat = int(size))]
+    letters_and_digits = ascii_lowercase+digits
+
+    print(all)
+
+    keywords = [''.join(i) for i in product(letters_and_digits, repeat = int(size))]
     count = 0
  
     print('Generated {} {} domain names'.format(len(keywords), tld))
